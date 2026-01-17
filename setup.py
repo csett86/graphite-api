a89e50f373021a939febec8858d80c92ddab4aaf
Christoph Settgast <csett86_git@quicksands.de>
2026-01-17 22:39:30 +0100

    remove obsolete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-"""
-Minimal setup.py shim for backward compatibility.
-All package metadata is now in pyproject.toml.
-"""
-from setuptools import setup
-
-# All configuration is now in pyproject.toml
-setup()
